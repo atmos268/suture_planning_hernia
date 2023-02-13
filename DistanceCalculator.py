503a0bbe90d73f65126296dedf3afaa9f77113ec
Harshika Jalan <harshikajalan@berkeley.edu>
2023-02-12 18:30:10 -0800

    distance calculation

--- DistanceCalculator.py
+++ DistanceCalculator.py
@@ -4,7 +4,7 @@ class DistanceCalculator():
         self.wound_width = wound_width
         pass
 
-    def calculate_distances(self, wound_points):
+    def calculate_distances(self, wound_points, curve):
         # wound points is the set of time-steps along the wound that correspond to wound points
 
         # Step 1: Calculate insertion and extraction points. Use the wound's
@@ -14,17 +14,37 @@ class DistanceCalculator():
         # Step 2: Between each adjacent pair of wound_points, with i
         # as wound point index, must calculate dist(i, i + 1) for
         # insert, center extract: refer to slide 13 for details:
+        
+        insert_points = []
+        center_points = []
+        extract_points = []
+        
+        def get_derivative(x):
+            d1 = curve.derivative()
+            return d1(x)
+        
+        def get_normal(x):
+
+
+        def euclidean_distance(point1, point2):
+            x1, y1, x2, y2 = point1[0], point1[1], point2[0], point2[1]
+            return sqrt((x2-x1)**2+(y2-y1)**2)
 
         def dist_insert(i):
-            pass
+            return euclidean_distance(insert_points[i], insert_points[i+1])
 
         def dist_center(i):
-            pass
+            return euclidean_distance(center_points[i], center_points[i+1])
 
         def dist_extract(i):
-            pass
+            return euclidean_distance(extract_points[i], extract_points[i+1])
+        
+        insert_distances = []
+        center_distances = []
+        extract_distances = []
+        for i in range(num_points-1):
+            insert_distances.append(dist_insert(i))
+            center_distances.append(dist_center(i))
+            extract_distances.append(dist_extract(i))
 
-        for all i:
-            calculate insert, center, extract distances
-
-        return 3 lists: insert dists, center dists, extract dists
+        return insert_distances, center_distances, extract_distances
