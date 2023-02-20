177696f1313c90587a847ff7256f9186bd783932
Yashish <ymmohnot@gmail.com>
2023-02-20 15:20:08 -0800

    made reward pytorch compatible

--- RewardFunction.py
+++ RewardFunction.py
@@ -1,8 +1,14 @@
+import torch as torch
+
 class RewardFunction():
     def __init__(self):
         self.insert_dists = []
         self.center_dists = []
         self.extract_dists = []
+        self.gradients = {}
+        self.gradients["center"] = 0
+        self.gradients["extract"] = 0
+        self.gradients["insert"] = 0
     
     # distance lists added to this object by SuturePlacer.
     # variance
@@ -21,6 +27,23 @@ class RewardFunction():
     #min - max
     def rewardA(self):
         return - (max(self.insert_dists) + max(self.center_dists) + max(self.extract_dists))
+    
+    def rewardX_torch(self):
+        insert_dists = torch.tensor(self.insert_dists, requires_grad=True)
+        center_dists = torch.tensor(self.center_dists, requires_grad=True)
+        extract_dists = torch.tensor(self.extract_dists, requires_grad=True)
+        
+        loss = -(torch.var(insert_dists) + torch.var(center_dists) + torch.var(extract_dists))
+        loss.backward()
+
+        self.gradients["center"] = center_dists.grad
+        self.gradients["extract"] = extract_dists.grad
+        self.gradients["insert"] = insert_dists.grad
+
+        return loss
+
+    def reward_gradient(self):
+        return self.gradients
 
 
     # ... and so forth: refer to slide 14 from the presentation for details on how to design this.
