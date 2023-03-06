a934669fbce06f2276fd96c52afea8a5c6ea797f
Harshika Jalan <harshikajalan@berkeley.edu>
2023-03-06 10:25:39 -0800

    latest changes

--- Constraints.py
+++ Constraints.py
@@ -10,14 +10,19 @@ class Constraints:
         h = 0.3
         return [i - h for i in insert_dists] + [i - h for i in center_dists] + [i - h for i in extract_dists]
     
-    def con3(self, t): # max distance b/w 2 sutures
-        insert_dists, center_dists, extract_dists, insert_pts, center_pts, extract_pts = self.DistanceCalculator.calculate_distances(t)   
-        h = 0.7
-        return [h - i for i in insert_dists] + [h - i for i in center_dists] + [h - i for i in extract_dists]
+    # def con3(self, t): # max distance b/w 2 sutures
+    #     insert_dists, center_dists, extract_dists, insert_pts, center_pts, extract_pts = self.DistanceCalculator.calculate_distances(t)   
+    #     h = 0.7
+    #     return [h - i for i in insert_dists] + [h - i for i in center_dists] + [h - i for i in extract_dists]
+
+    def con4(self, t):
+        return [t[1+1] - t[1] for i in range(len(t)-1)]
     
     def constraints(self):
         start = self.wound_points[0]
         end = self.wound_points[-1]
         return ({'type': 'eq', 'fun': lambda t: t[0] - start}, {'type': 'eq', 'fun': lambda t: t[-1] - end}, 
-               {'type': 'ineq', 'fun': lambda t: self.con2(t)}, {'type': 'ineq', 'fun': lambda t: self.con3(t)}
+               {'type': 'ineq', 'fun': lambda t: self.con2(t)},
+               #{'type': 'ineq', 'fun': lambda t: self.con3(t)},
+               {'type': 'ineq', 'fun': lambda t: self.con4(t)}, 
                )
