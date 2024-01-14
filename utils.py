509c17ba93242183c8fb83c5b8a9e63b1b5bb55e
virajramakrishnancode <virajramakrishnan@berkeley.edu>
2024-01-14 20:40:39 +0400

    test method of finding path length across point cloud using a*

--- utils.py
+++ utils.py
@@ -20,18 +20,6 @@ def click_points_simple(img):
     plt.show()
     return left_coords,right_coords
 
-# gets the euclidian distance from any two points in any dimension
-def euclidean_dist(pt1, pt2):
-    if len(pt1) != len(pt2):
-        raise IndexError("Mismatched size of points to compare")
-    
-    total = 0
-
-    for i in range(len(pt1)):
-        total += (pt1[i] - pt2[i]) ** 2
-    
-    return math.sqrt(total)
-
 def get_mm_per_pixel(start_pt, end_pt, mm_indicated):
     
     pix_dist = math.sqrt((start_pt[0] - end_pt[0])**2 + (start_pt[1] - end_pt[1])**2)
