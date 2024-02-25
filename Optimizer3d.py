ff2ff233c8a8e71199370b48eacd12f73eec2e37
Harshika Jalan <harshikajalan@berkeley.edu>
2024-02-24 16:23:17 -0800

    step up function

--- Optimizer3d.py
+++ Optimizer3d.py
@@ -1,3 +1,7 @@
+import numpy as np
+from SuturePlacement3d import SuturePlacement3d
+from MeshIngestor import MeshIngestor
+from test_force_model import get_plane_estimation, project_vector_onto_plane
 class Optimizer3d:
     """
     This class takes in a mesh object, and a spline on the surface of the wound, and optimizes the position of
@@ -9,9 +13,10 @@ class Optimizer3d:
     def __init__(self, mesh, spline, suture_width):
         self.mesh = mesh
         self.spline = spline
+        self.suture_width = suture_width 
         self.suture_placement = None
-
-    def generate_inital_placement(mesh, spline):
+    
+    def generate_inital_placement(self, mesh, spline):
         """
         This function should take in a mesh and a spline and output an initial placement of center, 
         insertion and extraction points (equally spaced works)
@@ -20,7 +25,64 @@ class Optimizer3d:
 
         returns a SuturePlacement3d object with spline and points
         """
-        pass
+        num_sutures_initial = 5 #TODO: modify later based on spline length
+        points_t_initial = np.linspace(0, 1, int(num_sutures_initial))
+        return self.generate_placement(mesh, spline, points_t_initial)
+        
+    def generate_placement(self, mesh, spline, points_t):
+        """
+        This function should take in a mesh and a spline and output center, 
+        insertion and extraction points (step up function)
+
+        returns a SuturePlacement3d object with spline and points
+        """
+        num_points = len(points_t)
+
+        spline_x, spline_y, spline_z = spline[0], spline[1], spline[2]
+        derivative_x, derivative_y, derivative_z = spline_x.derivative(), spline_y.derivative(), spline_z.derivative()
+
+        # get center points
+        center_points = [[spline_x[t], spline_y[t], spline_z[t]] for t in points_t]
+
+        # get derivative points
+        derivative_points = [[derivative_x[t], derivative_y[t], derivative_z[t]] for t in points_t]
+
+        #get tangent plane normal vectors
+        normal_vectors = [get_plane_estimation(center_points[i], mesh) for i in range(num_points)]
+
+        # project derivatives onto the tangent plane
+        derivative_vectors = [project_vector_onto_plane(derivative_points[i], normal_vectors[i]) for i in range(num_points)]
+
+        # normalize normal vectors and derivative vectors
+        normal_vectors = [self.normalize_vector(normal_vectors[i]) for i in range(num_points)]
+        derivative_vectors = [self.normalize_vector(derivative_vectors[i]) for i in range(num_points)]
+
+        # Insertion points = cross product 
+        insertion_points = [self.suture_width * np.cross(normal_vectors[i], derivative_vectors[i]) for i in range(num_points)]
+
+        # Extraction points = - cross product
+        extraction_points = [self.suture_width * -np.cross(normal_vectors[i], derivative_vectors[i]) for i in range(num_points)]
+
+        # create suture placement 3d object
+
+        suturePlacement3d = SuturePlacement3d(spline, center_points, insertion_points, extraction_points)
+        return suturePlacement3d
+    
+    def normalize_vector(self, vector):
+        """
+        Normalize a vector to have a unit length.
+        
+        Parameters:
+            vector (list or tuple): The vector to be normalized.
+            
+        Returns:
+            list: The normalized vector.
+        """
+        magnitude = np.sqrt(sum(component**2 for component in vector))
+        normalized_vector = [component / magnitude for component in vector]
+        return normalized_vector
+
+
 
     def loss(mesh, placement):
         """
@@ -36,10 +98,12 @@ class Optimizer3d:
         """
         Calculate forces acting along the wound at a point, due to the placement as a whole
         """
+        pass
 
     def calculate_closure_force(point, placement):
         """
         Calculate forces acting to close the wound at a point, due to the placement as a whole
         """
+        pass
 
 
