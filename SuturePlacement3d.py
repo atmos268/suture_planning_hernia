31a7c9cfe35b252554e3ab5376d3264908772852
Harshika Jalan <harshikajalan@berkeley.edu>
2024-02-24 16:25:48 -0800

    small change

--- SuturePlacement3d.py
+++ SuturePlacement3d.py
@@ -1,8 +1,7 @@
 class SuturePlacement3d:
-    def __init__(self, spline, center_points, insertion_pts, extraction_pts, points_t):
+    def __init__(self, spline, center_points, insertion_pts, extraction_pts):
         self.spline = spline
         self.center_pts = center_points
         self.insertion_pts = insertion_pts
         self.extraction_pts = extraction_pts
-        self.points_t = points_t
     
